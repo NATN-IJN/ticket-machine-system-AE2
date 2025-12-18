17851dec79820d0adeede505a4556553ef2b7767
IJN-7 <nathannnadi1@gmail.com>
2025-12-18 19:15:06 +0000

    Implemented AdminHuib skeleton

--- composeApp/src/jvmMain/kotlin/com/ticketmachine/service/AdminHub.kt
+++ composeApp/src/jvmMain/kotlin/com/ticketmachine/service/AdminHub.kt
@@ -1,4 +1,24 @@
 package com.ticketmachine.service
 
-class AdminHub {
+import com.ticketmachine.database.DatabaseManager
+import com.ticketmachine.domain.*
+
+class AdminHub(
+    private val database: DatabaseManager,
+    private val ticketMachine: TicketMachine
+) {
+    private var currentAdmin: Admin? = null
+
+    fun login(username: String, password: String): Boolean
+    fun setCurrentAdmin(admin: Admin) { currentAdmin = admin }
+
+    fun adminMenu()
+
+    fun viewDestination(name: String): Destination?
+    fun addDestination()
+    fun updateDestinationPrices(name: String)
+
+    fun addSpecialOffer(name: String)
+    fun searchSpecialOfferId(id: String)
+    fun confirmDelete()
 }
